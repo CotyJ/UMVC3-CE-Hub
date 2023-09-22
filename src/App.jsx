df235b54a9ae4408f42d7288e8abce1801763e4a
Coty Janeway <coty.janeway@gmail.com>
2023-09-22 14:35:01 -0700

    Add basic components and background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import CharacterDetails from './CharacterPage';
+import CharacterSelector from './CharacterSelector';
 
+import './styles/index.css';
 
 // Bootstrap object
 const bootstrapCSS = './styles/bootstrapCSS.js';
@@ -8,13 +12,31 @@ const bootstrapCSS = './styles/bootstrapCSS.js';
 // Background image
 const backgroundImage = require('../assets/umvcBackground.jpg'); // Import the image
 
-
 const App = () => {
-  console.log("App is working");
+  console.log('App is working');
 
   return (
-    <div className="background-image" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100%', height: '100vh' , padding: '0px', margin: '0px'}}>
-      App is working still
+    <div
+      // Background Image
+      className="background-image"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-3 col-md-12">
+            <CharacterSelector />
+          </div>
+          <div className="col-lg-9 col-md-12">
+            <CharacterDetails />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
