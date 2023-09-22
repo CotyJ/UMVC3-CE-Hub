64a3bfe997e0fd42f6360befd7a10774281e6129
Coty Janeway <coty.janeway@gmail.com>
2023-09-22 14:36:27 -0700

    Create basic component structure

---
+++ src/CharacterPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles/index.css';
+
+// import CharacterDetails from './CharacterDetails';
+// import CharacterMovelist from './CharacterMovelist';
+// import CharacterChangelog from './CharacterChangelog';
+
+export default function CharacterPage() {
+  return (
+    <div id="character-details">
+      <h1>Character Page</h1>
+
+      <div>Character Details page</div>
+      <div>Movelist page</div>
+      <div>Changelog page</div>
+
+      {/*
+      Add links to...
+      <CharacterDetails />
+      <CharacterMovelist />
+      <CharacterChangelog />
+       */}
+
+    </div>
+  );
+};
---
+++ src/CharacterSelector.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/index.css';
+
+
+export default function CharacterSelector() {
+  return (
+    <div id='character-selector' className=''>
+      <div>CharacterSelector</div>
+      <div>CE Home</div>
+      <div>Marvel</div>
+      <div>Capcom</div>
+    </div>
+  )
+}
