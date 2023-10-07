558a844290bd8699333cd92b15abfbb48f3482b4
Coty Janeway <coty.janeway@gmail.com>
2023-10-07 01:42:29 -0700

    restructure

--- src/server/peekResponse.js
+++ src/server/peekResponse.js
@@ -2,16 +2,18 @@ const path = require('path');
 const fs = require('fs');
 
 // Capture data to look at object
-const peekResponse = function(){
-
-    fs.writeFile(path.join(__dirname, '../../peek.json'), JSON.stringify(topPostsData), err => {
+const peekResponse = function () {
+  fs.writeFile(
+    path.join(__dirname, '../../peek.json'),
+    JSON.stringify(topPostsData),
+    (err) => {
       if (err) {
         console.log(err);
-      } else{
+      } else {
         console.log('Success');
       }
-    });
-
-}
+    }
+  );
+};
 
 module.exports = peekResponse;
