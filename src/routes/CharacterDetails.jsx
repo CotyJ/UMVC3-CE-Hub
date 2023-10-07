ce00a0a19f00079c398adb0bc4f30e6450bf07ef
Coty Janeway <coty.janeway@gmail.com>
2023-10-07 01:40:03 -0700

    refactor route for images

--- src/routes/CharacterDetails.jsx
+++ src/routes/CharacterDetails.jsx
@@ -3,19 +3,16 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 export default function CharacterDetails({ character }) {
-
-  //REFACTOR: Remove try/catch when images are complete
+  //TODO: Remove try/catch when images are complete
   try {
-    var characterImage = require(`../assets/images/${character.name}.jpg`);
+    var characterImage = require(`../assets/images/character_images/${character.name}.jpg`);
   } catch (err) {
     // console.log(err);
   }
 
-
   return (
     <div id="character-details">
       <div id="character-info">
-
         <h1 id="character-name">{character.name}</h1>
 
         <div className="character-links">
@@ -23,7 +20,6 @@ export default function CharacterDetails({ character }) {
           <Link to={`/${character.id}/moves`}>Moves</Link>
           <Link to={`/${character.id}/changelog`}>Changelog</Link>
         </div>
-
       </div>
       {/* TODO: fix image appearance */}
       {/* <img id="character-image"src={`${characterImage}`} alt={`${character.name}`}></img> */}
