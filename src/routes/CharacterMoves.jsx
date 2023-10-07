bdb6416bb919f56ee190c9eebac0a8c0cd48e7ae
Coty Janeway <coty.janeway@gmail.com>
2023-10-07 01:11:38 -0700

    outline moves layout

--- src/routes/CharacterMoves.jsx
+++ src/routes/CharacterMoves.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 export default function CharacterMoves({ moves }) {
-  var characterMovelist = moves;
+  const { assists, command_normals, health, hypers, specials } = moves;
 
-  console.log(moves);
   return (
     <div>
-      {/* {moves} */}
+      <div className="health">
+        <div>Health</div>
+        {health}
+      </div>
+
+      <div className="assists">
+        <div>Assists</div>
+        <div>{assists[0]}</div>
+        <div>{assists[1]}</div>
+        <div>{assists[2]}</div>
+      </div>
+
+      <div className="command-normals">
+        <div>Command Normals</div>
+      </div>
+
+      <div className="special-moves">
+        <div>Special Moves</div>
+      </div>
+
+      <div className="hyper-combos">
+        <div>Hyper Combos</div>
+      </div>
     </div>
-  )
+  );
 }
