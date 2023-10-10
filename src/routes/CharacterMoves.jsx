ad25f287cc0812d9df9f629bf4a6cd74f10f86d6
Coty Janeway <coty.janeway@gmail.com>
2023-10-09 19:50:43 -0700

    add symbols and classnames

--- src/routes/CharacterMoves.jsx
+++ src/routes/CharacterMoves.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
+import imageMapping from './dataMapping';
+
+
 
 export default function CharacterMoves({ moves }) {
   const { assists, command_normals, health, hypers, specials } = moves;
 
   // TODO: (Data Entry) Add empty properties to all characters for missing props (assists, etc)
+  // TODO: (Add Feature) Symbols for assists
+  // TODO: (Add Feature) Health Bar comparison
 
   return (
-    <div>
+    <div id='character-moves'>
       <div className="health">
         <div>Health</div>
         {health}
       </div>
 
       <div className="assists">
-        <div>Assists</div>
-        <div>{assists[0]}</div>
-        <div>{assists[1]}</div>
-        <div>{assists[2]}</div>
+        <h2 id='assists-banner'>Assists</h2>
+        <div className='move-name'>{assists[0]}</div>
+        <div className='move-name'>{assists[1]}</div>
+        <div className='move-name'>{assists[2]}</div>
       </div>
 
       <div className="command-normals">
-        <div>Command Normals</div>
+        <h2 id='command-normals-banner' >Command Normals</h2>
         {command_normals.map((cnormal) => (
-          <div key={`${cnormal.name}`}>
-            <div>{cnormal.name}</div>
+          <div key={`${cnormal.name}`} className='move-details'>
+            <div className='move-name'>{cnormal.name}</div>
             <div className="controller-button-inputs">
               {cnormal.input.map((input, index) => (
                 <div key={index} className="controller-input-button">
-                  {input}
+                  <img src={imageMapping[input]} alt={input}/>
                 </div>
               ))}
             </div>
@@ -36,14 +41,14 @@ export default function CharacterMoves({ moves }) {
       </div>
 
       <div className="special-moves">
-        <div>Special Moves</div>
+        <h2 id='special-moves-banner' >Special Moves</h2>
         {specials.map((special) => (
-          <div key={`${special.name}`}>
-            <div>{special.name}</div>
+          <div key={`${special.name}`} className='move-details'>
+            <div className='move-name'>{special.name}</div>
             <div className="controller-button-inputs">
               {special.input.map((input, index) => (
                 <div key={index} className="controller-input-button">
-                  {input}
+                  <img src={imageMapping[input]} alt={input}/>
                 </div>
               ))}
             </div>
@@ -52,15 +57,15 @@ export default function CharacterMoves({ moves }) {
       </div>
 
       <div className="hyper-combos">
-        <div>Hyper Combos</div>
+        <h2 id='hyper-moves-banner'>Hyper Combos</h2>
 
         {hypers.map((hyper) => (
-          <div key={`${hyper.name}`}>
-            <div>{hyper.name}</div>
+          <div key={`${hyper.name}`} className='move-details'>
+            <div className='move-name'>{hyper.name}</div>
             <div className="controller-button-inputs">
               {hyper.input.map((input, index) => (
                 <div key={index} className="controller-input-button">
-                  {input}
+                  <img src={imageMapping[input]} alt={input} className={`${input}-button`}/>
                 </div>
               ))}
             </div>
