ffde0cf64d058a032df3e5446258b1d92db6adec
Coty Janeway <coty.janeway@gmail.com>
2023-10-09 19:51:36 -0700

    mapper for button images

---
+++ src/routes/dataMapping.js
@@ -0,0 +1,35 @@
+import buttonP from '../assets/images/button_images/+.png';
+import button1 from '../assets/images/button_images/1.png';
+import button2 from '../assets/images/button_images/2.png';
+import button3 from '../assets/images/button_images/3.png';
+import button4 from '../assets/images/button_images/4.png';
+import button5 from '../assets/images/button_images/5.png';
+import button6 from '../assets/images/button_images/6.png';
+import button7 from '../assets/images/button_images/7.png';
+import button8 from '../assets/images/button_images/8.png';
+import button9 from '../assets/images/button_images/9.png';
+import buttonA from '../assets/images/button_images/A.png';
+import buttonL from '../assets/images/button_images/L.png';
+import buttonM from '../assets/images/button_images/M.png';
+import buttonH from '../assets/images/button_images/H.png';
+import buttonS from '../assets/images/button_images/S.png';
+
+const imageMapping = {
+'+': buttonP,
+'1': button1,
+'2': button2,
+'3': button3,
+'4': button4,
+'5': button5,
+'6': button6,
+'7': button7,
+'8': button8,
+'9': button9,
+'A': buttonA,
+'L': buttonL,
+'M': buttonM,
+'H': buttonH,
+'S': buttonS,
+}
+
+export default imageMapping;
